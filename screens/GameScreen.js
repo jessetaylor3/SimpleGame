9e4f914781aa2e56bbb666486a5fe76ae1c91b68
Jesse Taylor <114543736+jessetaylor3@users.noreply.github.com>
2023-09-15 13:17:47 -0600

    Gamescreen black dot not really working :)

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,11 +1,36 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { View, Animated, PanResponder, StyleSheet } from 'react-native';
 
 const GameScreen = () => {
+  const [altitude] = useState(new Animated.Value(0));
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (_, gestureState) => {
+        // Move the dot based on the touch movement
+        Animated.timing(altitude, {
+          toValue: -gestureState.dy, // Use gestureState to control altitude
+          duration: 16, // Adjust animation duration for smoothness
+          useNativeDriver: false, // Required for some animations
+        }).start();
+      },
+      onPanResponderRelease: () => {
+        // When user releases the screen, let the dot fall (reset altitude)
+        Animated.timing(altitude, {
+          toValue: 0, // Reset altitude to ground level
+          duration: 500, // Duration of the animation
+          useNativeDriver: false, // Required for some animations
+        }).start();
+      },
+    })
+  ).current;
+
   return (
     <View style={styles.container}>
-      <Text>This is the Game Screen</Text>
-      {/* Add your game content here */}
+      <Animated.View
+        style={[styles.dot, { bottom: altitude }]}
+        {...panResponder.panHandlers}
+      />
     </View>
   );
 };
@@ -13,8 +38,16 @@ const GameScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'white',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  dot: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'black',
+    position: 'absolute',
+    borderRadius: 25, // Make it a circle
   },
 });
 
--- screens/LockerScreen.js
+++ screens/LockerScreen.js
@@ -1,13 +1,16 @@
-// LockerScreen.js
-
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
-const LockerScreen = () => {
+const LockerScreen = ({ navigation }) => {
+  const navigateToHome = () => {
+    navigation.goBack(); // Go back to the previous screen, which is the "Home" screen
+  };
+
   return (
     <View style={styles.container}>
       <Text>This is the Locker Screen</Text>
       {/* Add your locker content here */}
+      <Button title="Home" onPress={navigateToHome} />
     </View>
   );
 };
