0da5a6b73a85670a66ebaf5f663319f49970edca
Seth_Dijkstra <119450434+SethDijkstra@users.noreply.github.com>
2023-11-29 15:45:19 -0700

    Plane size changed, physics changed

--- components/Plane.js
+++ components/Plane.js
@@ -6,10 +6,9 @@ import { Image } from 'react-native';
 const planeImage = require('../assets/images/bluePlane.png'); 
 
 const Plane = ({ body }) => {
-  console.log('Plane component is rendered');
   const { position } = body;
-  const width = 100;
-  const height = 100;
+  const width = 50;
+  const height = 50;
 
   return (
 
--- systems/Physics.js
+++ systems/Physics.js
@@ -1,20 +1,23 @@
 // systems/Physics.js
-const Physics = (entities, { touches, time }) => {
+import { Dimensions } from 'react-native';
+
+const Physics = (entities, { touches, time, dispatch }) => {
   let plane = entities.plane.body;
+  const screenHeight = Dimensions.get('window').height;
 
   // Apply gravity
-  plane.velocity.y += 0.2;
+  plane.velocity.y += 0.4; // Gravity
   plane.position.y += plane.velocity.y;
 
   // Stop the plane at the bottom of the screen
-  if (plane.position.y >= window.innerHeight - plane.size.height) {
-    plane.velocity.y = 0;
-    plane.position.y = window.innerHeight - plane.size.height;
+  if (plane.position.y + plane.size.height > screenHeight) {
+    plane.velocity.y = 1;
+    plane.position.y = screenHeight - plane.size.height;
     // Optionally dispatch 'game-over' event
     // dispatch({ type: 'game-over' });
   }
 
-  // Plane jump on touch anywhere
+  // Plane jump on touch
   touches.filter(t => t.type === 'press').forEach(t => {
     plane.velocity.y = -7; // Adjust the force of the jump
   });
