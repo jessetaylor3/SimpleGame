d34cbaf47b77ab85c701350803d65bf310817c16
Jesse Taylor <114543736+jessetaylor3@users.noreply.github.com>
2023-11-30 17:17:55 -0700

    Fixed positioning frfr

--- systems/ObstacleSystem.js
+++ systems/ObstacleSystem.js
@@ -27,8 +27,8 @@ const ObstacleSystem = (entities, { time }) => {
       position = { x: screenWidth + 100, y: screenHeight * 0.2}; // Airplane spawns in the sky
       size = { width: 100, height: 50 }; // Example size for airplane
     } else { // 'building'
-      position = { x: screenWidth + 100, y: 400 }; // Building spawns on the ground
       size = { width: 100, height: 500 }; // Example size for building
+      position = { x: screenWidth + 100, y: screenHeight - size.height }; // Building spawns on the ground
     }
 
     entities[newObstacleKey] = {
